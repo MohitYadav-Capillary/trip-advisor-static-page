d6957c167c8b83a10bda8d59d15cca9623e42e28
MohitYadav <mohityadav@CLPT1933.local>
2024-01-17 17:39:10 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,18 @@
+let x = 5;
+var y = 7;
+const z = 9;
+
+
+// console.log(z);
+
+// let arr = new Array("hey",2);
+
+// console.log(arr);
+
+let fruits = []; 
+
+fruits[2] = 5; 
+
+fruits.age = 25
+
+console.log(fruits[0]);
